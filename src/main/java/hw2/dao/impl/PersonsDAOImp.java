7e85c2d204f0b0dfc2b461144a850286752dd24a
Denis Gultay <denisgultay@gmail.com>
2017-07-13 21:41:09 +0300

    5th commit

---
+++ src/main/java/hw2/dao/impl/PersonsDAOImp.java
@@ -0,0 +1,56 @@
+package hw2.dao.impl;
+
+import hw2.dao.PersonsDAO;
+import hw2.entityes.Persons;
+
+import java.util.List;
+
+/**
+ * Created by Користувач on 13.07.2017.
+ */
+public class PersonsDAOImp implements PersonsDAO {
+    @Override
+    public List<Persons> findAll() {
+        return null;
+    }
+
+    @Override
+    public Persons findById(int personsId) {
+        return null;
+    }
+
+    @Override
+    public Persons findByName(String personsName) {
+        return null;
+    }
+
+    @Override
+    public Persons findByAge(int personsAge) {
+        return null;
+    }
+
+    @Override
+    public Persons findByCompanyId(int companyId) {
+        return null;
+    }
+
+    @Override
+    public Persons findBySalary(int salary) {
+        return null;
+    }
+
+    @Override
+    public void insert(Persons persons) {
+
+    }
+
+    @Override
+    public void update(Persons persons) {
+
+    }
+
+    @Override
+    public void delete(Persons persons) {
+
+    }
+}
---
+++ src/main/java/hw2/dao/impl/ProjectsDAOImp.java
@@ -0,0 +1,56 @@
+package hw2.dao.impl;
+
+import hw2.dao.ProjectsDAO;
+import hw2.entityes.Projects;
+
+import java.util.List;
+
+/**
+ * Created by Користувач on 13.07.2017.
+ */
+public class ProjectsDAOImp implements ProjectsDAO {
+    @Override
+    public List<Projects> findAll() {
+        return null;
+    }
+
+    @Override
+    public Projects findById(int projectsId) {
+        return null;
+    }
+
+    @Override
+    public Projects findByName(String projectsName) {
+        return null;
+    }
+
+    @Override
+    public Projects findByCustomersId(int customersId) {
+        return null;
+    }
+
+    @Override
+    public Projects findByCompanyId(int companyId) {
+        return null;
+    }
+
+    @Override
+    public Projects findByCosts(int projectCost) {
+        return null;
+    }
+
+    @Override
+    public void insert(Projects projects) {
+
+    }
+
+    @Override
+    public void update(Projects projects) {
+
+    }
+
+    @Override
+    public void delete(Projects projects) {
+
+    }
+}

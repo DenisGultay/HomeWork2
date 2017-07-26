7406006589adc2ce41b04cd897d86d41214658bf
Denis Gultay <denisgultay@gmail.com>
2017-07-27 00:50:26 +0300

    9 commit Даю на перевірку

--- src/main/java/hw2/Main.java
+++ src/main/java/hw2/Main.java
@@ -1,10 +1,7 @@
 package hw2;
 
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
-import hw2.dao.impl.CompanyDAOImp;
-import hw2.dao.impl.PersonsDAOImp;
-import hw2.dao.impl.ProjectsDAOImp;
-import hw2.dao.impl.SkillsDAOImp;
+import hw2.dao.impl.*;
 import hw2.entityes.*;
 
 import java.sql.Connection;
@@ -22,25 +19,41 @@ public class Main {
     //   private static int count = 2;
     public static void main(String[] args) throws Exception {
 
-    //    PersonsDAOImp personsDAOImp = new PersonsDAOImp();
-//        CompanyDAOImp companyDAOImp = new CompanyDAOImp();
-//        SkillsDAOImp skillsDAOImp = new SkillsDAOImp();
+        PersonsDAOImp personsDAOImp = new PersonsDAOImp();
+        CompanyDAOImp companyDAOImp = new CompanyDAOImp();
+        SkillsDAOImp skillsDAOImp = new SkillsDAOImp();
         ProjectsDAOImp projectsDAOImp = new ProjectsDAOImp();
-//        Company company = getCompanyInstance();
-//        companyDAOImp.delete(5);
-//        System.out.println(personsDAOImp.findBySalary(2000));
+        CustomersDAOImp customersDAOImp = new CustomersDAOImp();
 
-   //    Persons mark = getPersonsInstance();
-   //     personsDAOImp.insert(mark);
-      //  personsDAOImp.addSkillsToPersons(mark, 14);
+//      ---------------- Company -------------------
+
+        Company company = getCompanyInstance();
+        companyDAOImp.insert(company);
+
+//      ---------------- Customers -----------------
+
+        System.out.println(customersDAOImp.findById(5));
+
+//      ----------------- Skills -------------------
+
+        Skills skill = getSkillssInstance();
+        skillsDAOImp.update(skill);
+
+//      -------------- Persons --------------------
+
+        Persons mark = getPersonsInstance();
+//               personsDAOImp.insert(mark);
+        personsDAOImp.addSkillsToPersons(mark, 14);
+
+//      -------------- Projects -------------------
 
         Projects lastProject = getProjectsInstance();
-    //    projectsDAOImp.insert(lastProject);
-          projectsDAOImp.addPersonsToProject(lastProject, 18);
+//        projectsDAOImp.insert(lastProject);
+        projectsDAOImp.addPersonsToProject(lastProject, 18);
     }
 
     static Company getCompanyInstance() {
-        return new Company("");
+        return new Company("Oracle");
     }
 
     static Customers getCustomersInstance() {
@@ -49,20 +62,20 @@ public class Main {
 
     static Persons getPersonsInstance() {
         List<Integer> skillIdList = Arrays.asList(new Integer[]{1, 2, 5});
-        Persons persons = new Persons("Mark", 26, 4,5200);
+        Persons persons = new Persons("Mark", 26, 4, 5200);
         persons.setSkillsIDList(skillIdList);
         return persons;
     }
 
-    static Projects getProjectsInstance(){
+    static Projects getProjectsInstance() {
         List<Integer> personIdList = Arrays.asList(new Integer[]{7, 14});
-       Projects projects = new Projects("LastProject", 4, 3, 250000);
-       projects.setPersonsIDList(personIdList);
+        Projects projects = new Projects("LastProject", 4, 3, 250000);
+        projects.setPersonsIDList(personIdList);
         return projects;
     }
 
-    static Skills getSkillssInstance(){
-        return new Skills();
+    static Skills getSkillssInstance() {
+        return new Skills("C#");
     }
 
 
